66f428031a5414e25eec2cf3ace9dd32141dea90
Andrew Dinh <andrewd@benchling.com>
2021-01-01 21:26:18 -0800

    Add first strapi project

---
+++ first-strapi-app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b9c1f716846654aa868a000979904286'),
+    },
+  },
+});
---
+++ first-strapi-app/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a71f29e2-1840-42be-b8d9-c8a8e8f57684'
+};
