eb6671c41a4fe341bdd6cac3a9e01915822d4374
Andrew Dinh <andrewd@benchling.com>
2021-01-04 23:34:30 -0800

    Bring Post into App and render it

--- ig-frontend/src/App.js
+++ ig-frontend/src/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import Post from './components/Post';
 
 function App() {
   return (
     <div className="App">
+      <Post />
     </div>
   );
 }
