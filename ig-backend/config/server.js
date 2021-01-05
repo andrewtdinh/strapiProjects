7047d2ab8b6450b497f73f131fe6814f09abcf3b
Andrew Dinh <andrewd@benchling.com>
2021-01-04 22:39:17 -0800

    Create another project called ig-backend to play with Strapi

---
+++ ig-backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3ae3a65673158d49efce31b27fc4ef3a'),
+    },
+  },
+});
---
+++ ig-backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'baf78a8b-8c0f-4b2d-82a1-51f617af03e6'
+};
