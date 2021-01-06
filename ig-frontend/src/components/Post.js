080bf026b0fa2f810dfaf39f42092fd9fee976b3
Andrew Dinh <andrewd@benchling.com>
2021-01-05 21:58:46 -0800

    Modify Post component

--- ig-frontend/src/components/Post.js
+++ ig-frontend/src/components/Post.js
@@ -1,5 +1,78 @@
 // import React from 'react';
 
+const post = {
+  "id": 1,
+  "description": "This is an old picture of me in Panatnikhom.  This picture was taken when I was 12 years old and living in a refugee camp.",
+  "likes": 26,
+  "author": null,
+  "published_at": "2021-01-05T07:15:07.352Z",
+  "created_at": "2021-01-05T07:14:33.044Z",
+  "updated_at": "2021-01-05T07:15:07.380Z",
+  "image": {
+    "id": 1,
+    "name": "AndrewInPanatNiKhom.jpeg",
+    "alternativeText": "",
+    "caption": "",
+    "width": 2065,
+    "height": 2956,
+    "formats": {
+      "thumbnail": {
+        "name": "thumbnail_AndrewInPanatNiKhom.jpeg",
+        "hash": "thumbnail_Andrew_In_Panat_Ni_Khom_1117079e76",
+        "ext": ".jpeg",
+        "mime": "image/jpeg",
+        "width": 109,
+        "height": 156,
+        "size": 6.83,
+        "path": null,
+        "url": "/uploads/thumbnail_Andrew_In_Panat_Ni_Khom_1117079e76.jpeg"
+      },
+      "large": {
+        "name": "large_AndrewInPanatNiKhom.jpeg",
+        "hash": "large_Andrew_In_Panat_Ni_Khom_1117079e76",
+        "ext": ".jpeg",
+        "mime": "image/jpeg",
+        "width": 699,
+        "height": 1000,
+        "size": 136.99,
+        "path": null,
+        "url": "/uploads/large_Andrew_In_Panat_Ni_Khom_1117079e76.jpeg"
+      },
+      "medium": {
+        "name": "medium_AndrewInPanatNiKhom.jpeg",
+        "hash": "medium_Andrew_In_Panat_Ni_Khom_1117079e76",
+        "ext": ".jpeg",
+        "mime": "image/jpeg",
+        "width": 524,
+        "height": 750,
+        "size": 88.3,
+        "path": null,
+        "url": "/uploads/medium_Andrew_In_Panat_Ni_Khom_1117079e76.jpeg"
+      },
+      "small": {
+        "name": "small_AndrewInPanatNiKhom.jpeg",
+        "hash": "small_Andrew_In_Panat_Ni_Khom_1117079e76",
+        "ext": ".jpeg",
+        "mime": "image/jpeg",
+        "width": 349,
+        "height": 500,
+        "size": 46.04,
+        "path": null,
+        "url": "/uploads/small_Andrew_In_Panat_Ni_Khom_1117079e76.jpeg"
+      }
+    },
+    "hash": "Andrew_In_Panat_Ni_Khom_1117079e76",
+    "ext": ".jpeg",
+    "mime": "image/jpeg",
+    "size": 785.3,
+    "url": "/uploads/Andrew_In_Panat_Ni_Khom_1117079e76.jpeg",
+    "previewUrl": null,
+    "provider": "local",
+    "provider_metadata": null,
+    "created_at": "2021-01-05T07:12:16.155Z",
+    "updated_at": "2021-01-05T07:12:16.166Z"
+  }
+}
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => (
   <div className="Post">
