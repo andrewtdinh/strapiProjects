45825f7908e9cec5bc769ea26467425c10f5e3de
Andrew Dinh <andrewd@benchling.com>
2021-01-06 21:58:27 -0800

    Move the relevant post information to App.js

--- ig-frontend/src/App.js
+++ ig-frontend/src/App.js
@@ -2,12 +2,17 @@ import './App.css';
 import Post from './components/Post';
 
 function App() {
+  const post = {
+    "description": "This is an old picture of me in Panatnikhom.  This picture was taken when I was 12 years old and living in a refugee camp.",
+    "likes": 26,
+    "image": {
+      "url": "/uploads/Andrew_In_Panat_Ni_Khom_1117079e76.jpeg",
+    }
+  };
+
   return (
     <div className="App">
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post post={post} />
     </div>
   );
 }
