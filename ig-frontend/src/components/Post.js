746d17edbffdd9c5617fd57ae236d8149017cad3
Andrew Dinh <andrewd@benchling.com>
2021-01-07 22:28:45 -0800

    Refactor Post component to destructure the props properties

--- ig-frontend/src/components/Post.js
+++ ig-frontend/src/components/Post.js
@@ -2,10 +2,7 @@ const API_URL = 'http://localhost:1337';
 
 const formatImageUrl = (url) => `${API_URL}${url}`;
 // eslint-disable-next-line import/no-anonymous-default-export
-export default (props) => {
-  const url = props.post.image && props.post.image.url;
-  const { description, likes } = props.post;
-
+export default ({description, likes, url}) => {
   return (
     <div className="Post">
       <img className="Post__Image" src={formatImageUrl(url)} alt="post hero" />
