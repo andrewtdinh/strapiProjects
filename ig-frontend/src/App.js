b1d46730172739261a53ab60a6298fd386a1ee60
Andrew Dinh <andrewd@benchling.com>
2021-01-07 22:24:15 -0800

    Move the post object outside of App.js

--- ig-frontend/src/App.js
+++ ig-frontend/src/App.js
@@ -1,18 +1,23 @@
 import './App.css';
 import Post from './components/Post';
 
+const post = {
+  "description": "This is an old picture of me in Panatnikhom.  This picture was taken when I was 12 years old and living in a refugee camp.",
+  "likes": 26,
+  "image": {
+    "url": "/uploads/Andrew_In_Panat_Ni_Khom_1117079e76.jpeg",
+  }
+};
+
 function App() {
-  const post = {
-    "description": "This is an old picture of me in Panatnikhom.  This picture was taken when I was 12 years old and living in a refugee camp.",
-    "likes": 26,
-    "image": {
-      "url": "/uploads/Andrew_In_Panat_Ni_Khom_1117079e76.jpeg",
-    }
-  };
 
   return (
     <div className="App">
-      <Post post={post} />
+      <Post 
+        description={post.description}
+        likes={post.likes}
+        url={post.image.url}
+      />
     </div>
   );
 }
