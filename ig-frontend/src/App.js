3a96c65a6f04605dbc13e0f6e7ddba29e58277e2
Andrew Dinh <andrewd@benchling.com>
2021-01-08 22:00:33 -0800

    Refactor App.js to take in an array of posts

--- ig-frontend/src/App.js
+++ ig-frontend/src/App.js
@@ -1,23 +1,43 @@
 import './App.css';
 import Post from './components/Post';
 
-const post = {
-  "description": "This is an old picture of me in Panatnikhom.  This picture was taken when I was 12 years old and living in a refugee camp.",
-  "likes": 26,
-  "image": {
-    "url": "/uploads/Andrew_In_Panat_Ni_Khom_1117079e76.jpeg",
-  }
-};
+const posts = [
+  {
+    "description":  "The first post",
+    "likes": 26,
+    "image": {
+      "url": "/uploads/Andrew_In_Panat_Ni_Khom_1117079e76.jpeg",
+    }
+  },
+  {
+    "description":  "The second post",
+    "likes": 16,
+    "image": {
+      "url": "/uploads/Andrew_In_Panat_Ni_Khom_1117079e76.jpeg",
+    }
+  },
+  {
+    "description":  "The third post",
+    "likes": 33,
+    "image": {
+      "url": "/uploads/Andrew_In_Panat_Ni_Khom_1117079e76.jpeg",
+    }
+  },
+]
 
 function App() {
 
   return (
     <div className="App">
-      <Post 
-        description={post.description}
-        likes={post.likes}
-        url={post.image.url}
-      />
+      {posts.map((post) => {
+        return (
+          <Post 
+            description={post.description}
+            likes={post.likes}
+            url={post.image.url}
+          />
+        );
+      })}
     </div>
   );
 }
