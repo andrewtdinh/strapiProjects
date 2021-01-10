24b893c437b0442cbe1e2c698510c472acc740ed
Andrew Dinh <andrewd@benchling.com>
2021-01-09 22:21:15 -0800

    Set up BrowserRouter inside App component

--- ig-frontend/src/App.js
+++ ig-frontend/src/App.js
@@ -1,8 +1,17 @@
+import Home from './pages/Home';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <h2>App</h2>
+      <BrowserRouter>
+        <Switch>
+          <Route path='/' component={Home} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
