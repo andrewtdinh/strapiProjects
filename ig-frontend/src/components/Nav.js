77fbf311c625b737d65e7c1918f016ee8da60224
Andrew Dinh <andrewd@benchling.com>
2021-01-10 20:53:38 -0800

    Create Nav component

---
+++ ig-frontend/src/components/Nav.js
@@ -0,0 +1,10 @@
+import { NavLink } from 'react-router-dom';
+
+export default function Nav() {
+  return (
+    <div className="Nav">
+      <NavLink to="/" exact>Home</NavLink>
+      <NavLink to="/create" exact>Create</NavLink>
+    </div>
+  )
+}
