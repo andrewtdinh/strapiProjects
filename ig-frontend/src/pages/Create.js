455849d2fc7a0e07e687105a374a68064affda98
Andrew Dinh <andrewd@benchling.com>
2021-01-10 21:38:22 -0800

    Create the description input form

--- ig-frontend/src/pages/Create.js
+++ ig-frontend/src/pages/Create.js
@@ -1,7 +1,19 @@
+import { useState } from 'react';
+
 export default function Create() {
+  const [ description, setDescription ] = useState('');
+
   return (
     <div className="Create">
-      Create page
+      <h2>Create</h2>
+
+      <form>
+        <input 
+          placeholder="Description"
+          value={description}
+          onChange={(event) => setDescription(event.target.value)}
+        />
+      </form>
     </div>
   )
 }
