3260f3bca12bd43d5dfa59b0adf4431f3ddceafd
Andrew Dinh <andrewd@benchling.com>
2021-01-11 22:44:55 -0800

    Create handleSubmit function

--- ig-frontend/src/pages/Create.js
+++ ig-frontend/src/pages/Create.js
@@ -3,16 +3,33 @@ import { useState } from 'react';
 export default function Create() {
   const [ description, setDescription ] = useState('');
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const response = await fetch('http://localhost:1337/posts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        description
+      })
+    });
+
+    const data = await response.json();
+  }
+
   return (
     <div className="Create">
       <h2>Create</h2>
 
-      <form>
+      <form onSubmit={handleSubmit}>
         <input 
           placeholder="Description"
           value={description}
           onChange={(event) => setDescription(event.target.value)}
         />
+        <button>Submit</button>
       </form>
     </div>
   )
