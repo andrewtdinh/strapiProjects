02de2c01b629ac6b30e9ee295191e014c9b9e981
Andrew Dinh <andrewd@benchling.com>
2021-01-13 23:18:31 -0800

    Set default likes to zero upon create

--- ig-backend/api/post/controllers/post.js
+++ ig-backend/api/post/controllers/post.js
@@ -17,9 +17,9 @@ module.exports = {
     let entity;
     if (ctx.is('multipart')) {
       const { data, files } = parseMultipartData(ctx);
-      entity = await strapi.services.post.create(data, { files });
+      entity = await strapi.services.post.create({ ...data, likes: 0 }, { files });
     } else {
-      entity = await strapi.services.post.create(ctx.request.body);
+      entity = await strapi.services.post.create({ ...ctx.request.body, likes: 0 });
     }
     return sanitizeEntity(entity, { model: strapi.models.post });
   },
