1d6eee80db08be7b44a308234fa86d29b7b1f718
Andrew Dinh <andrewd@benchling.com>
2021-01-14 21:01:02 -0800

    Throw a 400 error when a picture is not submitted

--- ig-backend/api/post/controllers/post.js
+++ ig-backend/api/post/controllers/post.js
@@ -19,7 +19,7 @@ module.exports = {
       const { data, files } = parseMultipartData(ctx);
       entity = await strapi.services.post.create({ ...data, likes: 0 }, { files });
     } else {
-      entity = await strapi.services.post.create({ ...ctx.request.body, likes: 0 });
+      ctx.throw(400, 'You must submit a multi-part request!')
     }
     return sanitizeEntity(entity, { model: strapi.models.post });
   },
