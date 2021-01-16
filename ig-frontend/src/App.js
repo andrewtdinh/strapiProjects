d100d264acd81437f2d58062b146936296a9266a
Andrew Dinh <andrewd@benchling.com>
2021-01-15 20:55:47 -0800

    Add another route for SinglePost

--- ig-frontend/src/App.js
+++ ig-frontend/src/App.js
@@ -4,6 +4,7 @@ import Nav from './components/Nav';
 
 import Home from './pages/Home';
 import Create from './pages/Create';
+import SinglePost from './pages/SinglePost';
 
 
 import './App.css';
@@ -17,6 +18,7 @@ function App() {
         <Switch>
           <Route path='/' exact component={Home} />
           <Route path='/create' exact component={Create} />
+          <Route path='/:id' exact component={SinglePost} />
         </Switch>
       </BrowserRouter>
     </div>
