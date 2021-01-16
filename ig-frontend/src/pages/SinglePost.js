bca65497335a7964da3ded511b745775a1aef9be
Andrew Dinh <andrewd@benchling.com>
2021-01-15 22:02:25 -0800

    Use useEffect in SinglePost to fetch the post daa

--- ig-frontend/src/pages/SinglePost.js
+++ ig-frontend/src/pages/SinglePost.js
@@ -1,6 +1,19 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
 
-export default function SinglePost() {
+export default function SinglePost({match}) {
+  const { id } = match.params;
+  const [ post, setPost ] = useState({});
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      const response = await fetch(`http://localhost:1337/posts/${id}`);
+      const data = await response.json();
+      console.log({data});
+      setPost(data);
+    }
+    fetchPost();
+  });
+
   return (
     <div>
       Single Post
