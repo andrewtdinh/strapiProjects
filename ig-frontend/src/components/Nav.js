0730a3ee25b81dda703340d7d01a46eed8d92f89
Andrew Dinh <andrewd@benchling.com>
2021-01-19 23:10:30 -0800

    Bring UserContext into Nav Bar

--- ig-frontend/src/components/Nav.js
+++ ig-frontend/src/components/Nav.js
@@ -1,6 +1,11 @@
 import { NavLink } from 'react-router-dom';
+import { useContext } from 'react';
+
+import UserContext from '../context/UserContext';
 
 export default function Nav() {
+  const { user } = useContext(UserContext);
+  
   return (
     <div className="Nav">
       <NavLink to="/" exact>Home</NavLink>
