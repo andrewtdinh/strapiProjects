e6901e748eeb9359701f967c60b62ec4ee6defdd
Andrew Dinh <andrewd@benchling.com>
2021-01-20 23:18:49 -0800

    Set up a barebone Login form

--- ig-frontend/src/pages/Login.js
+++ ig-frontend/src/pages/Login.js
@@ -1,8 +1,26 @@
+import { useState } from 'react';
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
+
   return (
     <div>
-      Login
+      <h2>Login</h2>
+
+      <form> 
+        <input 
+          type="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <input 
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <button>Login</button>
+      </form>
     </div>
   )
 }
