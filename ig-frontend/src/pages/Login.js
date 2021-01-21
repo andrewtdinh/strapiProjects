381a524afa62d170a2ec65bbe288089a6b00eb01
Andrew Dinh <andrewd@benchling.com>
2021-01-20 23:30:27 -0800

    Define handleSubmit for Login form

--- ig-frontend/src/pages/Login.js
+++ ig-frontend/src/pages/Login.js
@@ -4,11 +4,28 @@ export default () => {
   const [ email, setEmail ] = useState('');
   const [ password, setPassword ] = useState('');
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    const response = await fetch('http://localhost:1337/auth/local/', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        identifier: email,
+        password
+      })
+    });
+    
+    const data = await response.json();
+  }
+
   return (
     <div>
       <h2>Login</h2>
 
-      <form> 
+      <form onSubmit={handleSubmit}> 
         <input 
           type="email"
           value={email}
