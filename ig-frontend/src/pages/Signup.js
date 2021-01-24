4c77f92da163102adab491a01360c54992ac32dd
Andrew Dinh <andrewd@benchling.com>
2021-01-23 17:39:07 -0800

    Add local state management hook to Signup Page

--- ig-frontend/src/pages/Signup.js
+++ ig-frontend/src/pages/Signup.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default () => {
+  const [ email, setEmail ] = useState('');
+  const [ password, setPassword ] = useState('');
+  const [ error, setError ] = useState('');
+  
   return (
     <div>
-      Signup Page
+      <h2>Signup Page</h2>
+
+      <form>
+        <input 
+        
+        />
+        <input 
+        
+        />
+      </form>
     </div>
   )
 }
