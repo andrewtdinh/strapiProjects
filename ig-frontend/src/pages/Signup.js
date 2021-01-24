3e3ca1cac56fcb32cad7fe19569512db7df48094
Andrew Dinh <andrewd@benchling.com>
2021-01-23 20:08:48 -0800

    Able to register new user from Signup page

--- ig-frontend/src/pages/Signup.js
+++ ig-frontend/src/pages/Signup.js
@@ -21,7 +21,11 @@ export default () => {
           password
         })
       });
-      
+
+      const data = await response.json();
+
+      console.log({data})
+
     } catch (err) {
       setError('Somthing went wrong during signup: ' + err)
     }
