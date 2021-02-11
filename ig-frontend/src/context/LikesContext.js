7fce4cd864b14a4fd082cb1ece68b97d757d6b73
Andrew Dinh <andrewd@benchling.com>
2021-02-10 22:54:07 -0800

    Create and call method to load likes received in useEffect

--- ig-frontend/src/context/LikesContext.js
+++ ig-frontend/src/context/LikesContext.js
@@ -6,7 +6,8 @@ export const LikesContext = createContext(null);
 // eslint-disable-next-line import/no-anonymous-default-export
 export default ({ children }) => {
   const { user } = useContext(UserContext);
-  const [ likesGiven, setLikesGiven ] = useState([])
+  const [ likesGiven, setLikesGiven ] = useState([]);
+  const [ likesReceived, setLikesReceived ] = useState([]);
 
   useEffect(() => {
     if (user) {
@@ -19,6 +20,18 @@ export default ({ children }) => {
         const data = await response.json();
         setLikesGiven(data);
       }
+      loadLikesGiven();
+
+      const loadLikesReceived = async () => {
+        const response = await fetch(`http://localhost:1337/likes/received?post.author=${user.user.id}`, {
+          headers: {
+            'Authorization': `Bearer ${user.jwt}`
+          }
+        })
+        const data = await response.json();
+        setLikesReceived(data);
+      }
+      loadLikesReceived();
     }
   }, [user])
 
