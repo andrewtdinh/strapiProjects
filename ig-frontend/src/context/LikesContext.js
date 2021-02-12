2b7213b3e2dcbad3f87da0e4903558f12b11c0ff
Andrew Dinh <andrewd@benchling.com>
2021-02-11 22:44:24 -0800

    Create a reloader function and pass it down the LikesContextProvider

--- ig-frontend/src/context/LikesContext.js
+++ ig-frontend/src/context/LikesContext.js
@@ -9,34 +9,38 @@ export default ({ children }) => {
   const [ likesGiven, setLikesGiven ] = useState([]);
   const [ likesReceived, setLikesReceived ] = useState([]);
 
+  const reloader = () => {
+    const loadLikesGiven = async () => {
+      const response = await fetch(`http://localhost:1337/likes/given?user=${user.user.id}`, {
+        headers: {
+          'Authorization': `Bearer ${user.jwt}`
+        }
+      });
+      const data = await response.json();
+      setLikesGiven(data);
+    }
+    loadLikesGiven();
+
+    const loadLikesReceived = async () => {
+      const response = await fetch(`http://localhost:1337/likes/received?post.author=${user.user.id}`, {
+        headers: {
+          'Authorization': `Bearer ${user.jwt}`
+        }
+      })
+      const data = await response.json();
+      setLikesReceived(data);
+    }
+    loadLikesReceived();
+  }
+
   useEffect(() => {
     if (user) {
-      const loadLikesGiven = async () => {
-        const response = await fetch(`http://localhost:1337/likes/given?user=${user.user.id}`, {
-          headers: {
-            'Authorization': `Bearer ${user.jwt}`
-          }
-        });
-        const data = await response.json();
-        setLikesGiven(data);
-      }
-      loadLikesGiven();
-
-      const loadLikesReceived = async () => {
-        const response = await fetch(`http://localhost:1337/likes/received?post.author=${user.user.id}`, {
-          headers: {
-            'Authorization': `Bearer ${user.jwt}`
-          }
-        })
-        const data = await response.json();
-        setLikesReceived(data);
-      }
-      loadLikesReceived();
+      reloader();
     }
   }, [user])
 
   return (
-    <LikesContext.Provider value={{ likesGiven, likesReceived }}>
+    <LikesContext.Provider value={{ likesGiven, likesReceived, reloader }}>
       { children }
     </LikesContext.Provider>
   )
